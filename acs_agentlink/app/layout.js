9c79bb7478058a73a1ac6f162e7f8ddca57641e1
viktohblake <damanygt@gmail.com>
2024-09-04 18:43:04 +0100

    setup project

---
+++ acs_agentlink/app/layout.js
@@ -0,0 +1,17 @@
+import { Outfit } from "next/font/google";
+import "./globals.css";
+
+const inter = Outfit({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "A.C.S. AGENTLINK",
+  description: "Elevate your prop firm with expert support agents",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={outfit.className}>{children}</body>
+    </html>
+  );
+}
