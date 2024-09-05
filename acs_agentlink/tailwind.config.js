416f9e095fd390e9f83dfd8dcf3933f6694d6a2c
viktohblake <damanygt@gmail.com>
2024-09-05 07:42:30 +0100

    set color palette

--- acs_agentlink/tailwind.config.js
+++ acs_agentlink/tailwind.config.js
@@ -4,14 +4,54 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      colors: {
+        grayscale: {
+          background: '#FCFCFC',
+          background_weak: '',
+          input: '',
+          line: '#D9DBE9',
+          placeholder: '#A0A3BD',
+          label: '#6E7191',
+          body: '',
+          header_weak: '#262338',
+          header: '#14142B',
+          title_active: '',
+          white: '#FFFFFF',
+        },
+        primary: {
+          background: '',
+          background_strong: '',
+          DEFAULT: '#FA8507',  // Default color
+          weak: '', // Light shade for weaker emphasis
+          strong: '', // Darker shade for stronger emphasis
+        },
+        secondary: {
+          DEFAULT: '',
+          weak: '',
+          strong: '',
+        },
+        error: {
+          DEFAULT: '',
+          weak: '',
+          strong: '',
+        },
+        success: {
+          DEFAULT: '',
+          weak: '',
+          strong: '',
+        },
+        warning: {
+          DEFAULT: '',
+          background: '',
+          background_strong: '',
+          weak: '',
+          strong: '',
+        }
+      }
     },
   },
   plugins: [],
