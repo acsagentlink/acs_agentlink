8e977b353d8a6c1f791229a34b77da0d96f26aa5
viktohblake <damanygt@gmail.com>
2024-11-26 01:03:17 +0100

    some update on hire agent

--- acs_agentlink/src/app/hire-an-agent/page.js
+++ acs_agentlink/src/app/hire-an-agent/page.js
@@ -9,7 +9,7 @@ export default function HireAgent() {
     return (
         <div className="bg-grayscale-white w-full min-h-screen overflow-hidden">
             <Header />
-            <div className="flex justify-between gap-20 px-0 sm:px-4 md:px-10 lg:px-20">
+            <div className="flex justify-between gap-20 px-0 sm:px-4 md:px-10 lg:pl-20 lg:pr-[7rem]">
                 <div className="hidden md:block flex-1 -ml-10 lg:-ml-10 md:ml-2">
                     <Image
                         src={FormImage}
@@ -18,8 +18,9 @@ export default function HireAgent() {
                     />
                 </div>
 
-                <div className="p-5 sm:p-0 flex-1 max-w-lg lg:pr-16 lg:mt-16 mt:mb-16">
-                    <p className="text-[#101828] text-5xl font-semibold">Hire an<br />agent</p>
+                <div className="m-auto p-5 sm:p-0 flex-1 md:max-w-lg max-w-[32rem] lg:mt-16 mt:mb-16">
+                    <p className="text-[#101828] text-5xl font-semibold">Hire an agent</p>
+                    <p className="text-grayscale-label">Let us know exactly what you need, and we’ll connect you with skilled agents ready to deliver exceptional service.</p>
                     <HireAgentForm />
                 </div>
             </div>
--- acs_agentlink/src/components/hire-agent/Form.js
+++ acs_agentlink/src/components/hire-agent/Form.js
@@ -1,127 +1,198 @@
-import { useForm, FormProvider } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { FormDataSchema } from './FormSchema';
-import axios from 'axios';
-import { useState } from 'react';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import { Button } from '@/components/ui/button';
-import Image from 'next/image';
-import MoonIcon from '../../../public/moon.svg';
-import SunIcon from '../../../public/sun.svg';
-import SunFogIcon from '../../../public/sun-fog.svg';
-import { useRouter } from 'next/navigation';
+"use client";
 
-export default function HireAgentForm() {
-  const [errorMessage, setErrorMessage] = useState(); 
+import { useState } from "react";
+import { useForm, FormProvider } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { FormDataSchema } from "./FormSchema";
+import axios from "axios";
+import Step1 from "./Step1";
+import Step2 from "./Step2";
+import { motion } from "framer-motion";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+
+export default function HireForm() {
+  const [currentStep, setCurrentStep] = useState(0);
+  const [errorMessage, setErrorMessage] = useState();
+
   const router = useRouter();
 
+  const [loading, setLoading] = useState(false);
+
   const methods = useForm({
+    defaultValues: {
+      emailSupportCombined: [],
+      liveChatCombined: [],
+      socialMediaCombined: [],
+      otherPlatformsCombined: [],
+    },
     resolver: zodResolver(FormDataSchema),
-    mode: 'onBlur'
+    mode: "onBlur",
   });
 
-  const { handleSubmit, watch, setValue, register, formState: { errors }, control, trigger } = methods;
-  const [selectedTime, setSelectedTime] = useState(watch('workingHours', '3pm - 11pm')); // Default to '3pm - 11pm'
-
-  const handleTimeSelection = (time, event) => {
-    event.preventDefault();
-    setSelectedTime(time);
-    setValue('workingHours', time);
-  };
+  const { handleSubmit, trigger, watch } = methods;
 
   const processForm = async (data) => {
+    setLoading(true);
     const formData = new FormData();
 
     // Append form fields to FormData
-    formData.append('name', data.name);
-    formData.append('email', data.email);
-    formData.append('no_of_agents', data.no_of_agents);
-    formData.append('workingHours', data.preferredTime);
+    formData.append("name", data.name);
+    formData.append("email", data.email);
+    formData.append("password", data.password);
+    formData.append("phone_number", data.whatsapp_number);
+    formData.append("telegram", data.telegram);
+    formData.append("location", data.location);
+    formData.append("another_job", data.another_job);
+    formData.append("job_description", data.job_description);
+    formData.append("availability", data.availability);
+
+    formData.append("support_experience", data.support_experience);
+    formData.append("support_experience_description", data.support_experience_description);
+    formData.append("email_support", JSON.stringify(data.emailSupportCombined)); 
+    formData.append("live_chat", JSON.stringify(data.liveChatCombined)); 
+    formData.append("social_media_support", JSON.stringify(data.socialMediaCombined)); 
+    formData.append("other_platforms", JSON.stringify(data.otherPlatformsCombined)); 
 
     try {
-      await axios.post('/api/submit', formData);
-      router.push('/hire-an-agent/success');
+      console.log(data)
+      await axios.post("/api/hire-an-agent", formData);
+      router.push("/hire-an-agent/success");
     } catch (error) {
-      const apiError = error.response?.data?.error || error.response?.data?.message || 'An unexpected error occurred.';
+      setLoading(false);
+      const apiError =
+        error.response?.data?.error ||
+        error.response?.data?.message ||
+        "An unexpected error occurred.";
       setErrorMessage(apiError);
+    }
+  };
+
+  const next = async () => {
+    const fields = steps[currentStep].fields;
+    const output = await trigger(fields, { shouldFocus: true });
+
+    if (!output) {
+      console.error("Validation failed for fields:", methods.formState.errors);
+      return;
+    }
+
+    if (output) {
+      if (currentStep < steps.length - 1) {
+        setCurrentStep(currentStep + 1);
+      } else {
+        handleSubmit(processForm)();
+      }
+    }
+  };
+
+  const prev = () => {
+    if (currentStep > 0) {
+      setCurrentStep(currentStep - 1);
     }
   };
 
   return (
     <FormProvider {...methods}>
-      <section className='flex flex-col justify-between text-[#101828] sm:w-none lg:w-full'>
+      <section className="flex flex-col justify-between text-[#101828] pb-10">
+        {/* Steps Indicator */}
+        <nav aria-label="Progress" className="w-[100px] mt-5">
+          <div className="flex items-center gap-4">
+            {steps.map((step, index) => (
+              <div key={step.id} className="flex-1">
+                <div className="relative">
+                  {/* Step Connector */}
+                  {index < steps.length - 1 && (
+                    <div
+                      className={`absolute top-1/2 left-full transform -translate-y-1/2 w-full border-t-2 ${
+                        currentStep > index
+                          ? "border-grayscale-header_weak"
+                          : "border-gray-200"
+                      }`}
+                      style={{ zIndex: -1 }}
+                    />
+                  )}
+                  {/* Step Line */}
+                  <div
+                    className={`w-full h-1 rounded-full ${
+                      currentStep >= index
+                        ? "bg-grayscale-header_weak"
+                        : "bg-gray-200"
+                    }`}
+                    style={{ height: "4px" }}
+                  />
+                </div>
+              </div>
+            ))}
+          </div>
+        </nav>
 
-        {/* Single screen form */}
-        <form className='py-8' onSubmit={handleSubmit(processForm)}>
-          <div className="space-y-5">
-            <div className='space-y-2'>
-              <Label className="text-[#344054]" htmlFor="companyName">Company name</Label>
-              <Input id="companyName" {...register('companyName')} placeholder="Enter your company name" className="focus:outline-none focus:ring-2 focus:ring-grayscale-header_weak"/>
-              {errors.companyName && <p className='text-red-500'>{errors.companyName.message}</p>}
-            </div>
-
-            <div className='space-y-2'>
-              <Label className="text-[#344054]" htmlFor="companyEmail">Company email</Label>
-              <Input id="companyEmail" {...register('companyEmail')} placeholder="Enter your company email" className="focus:outline-none focus:ring-2 focus:ring-grayscale-header_weak" />
-              {errors.companyEmail && <p className='text-red-500'>{errors.companyEmail.message}</p>}
-            </div>
-
-            <div className='space-y-2'>
-              <Label className="text-[#344054]" htmlFor="number_of_agents">Number of agents</Label>
-              <Input id="number_of_agents" {...register('number_of_agents')} placeholder="How many agents do you need?" className="focus:outline-none focus:ring-2 focus:ring-grayscale-header_weak" />
-              <p className="text-primary-background_strong text-sm mb-4">476 agents available</p>
-              {errors.number_of_agents && <p className='text-red-500'>{errors.number_of_agents.message}</p>}
-            </div>
-
-
-            <div className='space-y-4 mt-2'>
-            <Label className="text-[#344054]" htmlFor="workingHours">Desired working hours</Label>
-<div className='flex space-x-4'>
-  <Button 
-              type='button'
-                className={`px-4 py-2 rounded-full hover:bg-grayscale-header_weak hover:text-white text-grayscale-label ${selectedTime === '8am - 3pm' ? 'bg-grayscale-header_weak text-white' : 'bg-grayscale-background_weak'}`}
-                onClick={(event) => handleTimeSelection('8am - 3pm', event)}
-              >
-                <Image src={MoonIcon} alt='8am - 3pm Icon' className='w-5 h-5 mr-2' />
-                8am - 3pm
-              </Button>
-              <Button 
-              type='button'
-                className={`px-4 py-2 rounded-full hover:bg-grayscale-header_weak hover:text-white text-grayscale-label ${selectedTime === '3pm - 11pm' ? 'bg-grayscale-header_weak text-white' : 'bg-grayscale-background_weak'}`}
-                onClick={(event) => handleTimeSelection('3pm - 11pm', event)}
-              >
-                <Image src={SunIcon} alt='3pm - 11pm Icon' className='w-5 h-5 mr-2' />
-                3pm - 11pm
-              </Button>
-              <Button 
-              type='button'
-                className={`px-4 py-2 rounded-full hover:bg-grayscale-header_weak hover:text-white text-grayscale-label ${selectedTime === 'Both' ? 'bg-grayscale-header_weak text-white' : 'bg-grayscale-background_weak'}`}
-                onClick={(event) => handleTimeSelection('Both', event)}
-              >
-                <Image src={SunFogIcon} alt='Both Icon' className='w-5 h-5 mr-2' />
-                Both
-              </Button>
-</div>
-              
-            </div>
-</div>
-
-          {/* Display error message below the form if there is any */}
-          {errorMessage && (
-            <div className='text-red-500 mt-2'>
-              {errorMessage}
-            </div>
+        {/* Form */}
+        <form className="py-10">
+          {currentStep === 0 && (
+            <motion.div
+              initial={{ x: "50%", opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 0.3, ease: "easeInOut" }}
+            >
+              <Step1 />
+            </motion.div>
           )}
 
-          {/* Submit Button */}
-          <div className='pt-10'>
-            <Button type="submit" className='w-full btn-primary rounded-full p-8 text-md text-grayscale-white'>
-              Send request
+          {currentStep === 1 && (
+            <motion.div
+              initial={{ x: "50%", opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 0.3, ease: "easeInOut" }}
+            >
+              <Step2 />
+            </motion.div>
+          )}
+        </form>
+
+        {/* Display error message below the form if there is any */}
+        {errorMessage && (
+          <div className="text-red-500 mt-2 break-words">{errorMessage}</div>
+        )}
+        {/* Navigation */}
+          <div className="flex justify-between gap-5">
+            {currentStep > 0 && (
+              <button
+                onClick={prev}
+                className="w-full btn-secondary rounded-full border border-grayscale-line"
+              >
+                Back
+              </button>
+            )}
+            <Button
+              onClick={next}
+              className="w-full btn-primary rounded-full p-8 text-md text-grayscale-white"
+            >
+              {loading ? (
+                <div className="spinner"></div>
+              ) : currentStep === steps.length - 1 ? (
+                "Submit"
+              ) : (
+                "Next"
+              )}
             </Button>
           </div>
-        </form>
       </section>
     </FormProvider>
   );
 }
+
+const steps = [
+  {
+    id: "1",
+    fields: [
+      
+    ],
+  },
+  {
+    id: "2",
+    fields: [
+      
+    ],
+  },
+];
