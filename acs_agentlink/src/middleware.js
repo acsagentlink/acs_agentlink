6336819950a15d6ceea7dfa035b45833f4016cae
viktohblake <damanygt@gmail.com>
2024-12-24 15:41:34 +0100

    public access restriction

--- acs_agentlink/src/middleware.js
+++ acs_agentlink/src/middleware.js
@@ -59,5 +59,5 @@ export function middleware(req) {
 }
 
 export const config = {
-  matcher: ['/', '/dashboard/:path*', '/become-an-agent/success', '/hire-an-agent/success'],
+  matcher: ['/:path*', '/dashboard/:path*', '/become-an-agent/success', '/hire-an-agent/success'],
 };
